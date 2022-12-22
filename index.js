7fc4a2441479e2fe1925c62c06911193e5685c3d
aseelflihan <aseelflihan@gmail.com>
2022-12-23 02:08:33 +0300

    complate assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+
+
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply) ;
+
+
+let random = Math.ceil(Math.random() * 100);
+
+let num3 = 10;
+let num4 = 6;
+let mod =  num3 % num4;
+
+console.log(mod); 
+
+
+let max = Math.max(10, 20, 12, 15);
+console.log(max); 
